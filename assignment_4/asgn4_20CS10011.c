93065e3280015d3d31e2ff98d0a3810e33fd7315
sneaky-potato <ashwanikamal.im421@gmail.com>
2022-10-15 16:11:28 +0530

    Add header in main driver file

--- assignment_4/asgn4_20CS10011.c
+++ assignment_4/asgn4_20CS10011.c
@@ -2,6 +2,14 @@
 extern int yyparse();
 extern int yylex();
 
+//#########################################
+//## Ashish Rekhani (20CS10010)          ##
+//## Ashwani Kumar Kamal (20CS10011)     ##
+//## Compilers Laboratory                ##
+//## Assignment - 3                      ##
+//#########################################
+//# GCC version: gcc (GCC) 12.1.1 20220730
+
 int main() {
     printf("-------------------- Parsing --------------------\n\n");
     yyparse();
