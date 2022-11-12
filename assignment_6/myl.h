62d1dc5aa709e3cd5e1b35b3b91880950ed8b63e
sneaky-potato <ashwanikamal.im421@gmail.com>
2022-11-12 10:58:17 +0530

    Add test function header file and test files

---
+++ assignment_6/myl.h
@@ -0,0 +1,21 @@
+//#########################################
+//## Ashish Rekhani (20CS10010)          ##
+//## Ashwani Kumar Kamal (20CS10011)     ##
+//## Compilers Laboratory                ##
+//## Assignment - 6                      ##
+//#########################################
+//# GCC version: gcc (GCC) 12.1.1 20220730
+
+#ifndef MYL_H
+#define MYL_H
+
+// Status codes
+#define ERR 1
+#define OK 0
+
+// Definition of functions used
+int printStr(char *);
+int printInt(int);
+int readInt(int *);
+
+#endif
